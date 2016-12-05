7224bc398747c20284e88c28d0a8c656b222aae3
ash-kai <nautiyal-ashish@live.com>
2016-12-05 07:59:51 -0800

    initial structure for miscellaneous tasks

---
+++ drchrono/utility.py
@@ -0,0 +1,7 @@
+#To perform various miscellaneous tasks
+
+def validatePatient(parameter):
+	# to Validate a patient for doctor's appointment
+
+def getTodaysAppointments():
+	# To get current days appointments
