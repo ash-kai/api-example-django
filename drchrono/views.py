de7831dd9df986ef72e60b6eb361e2d4f0496c56
ash-kai <nautiyal-ashish@live.com>
2016-12-05 07:58:38 -0800

    Initial structure of view and urls

--- drchrono/views.py
+++ drchrono/views.py
@@ -1,1 +1,17 @@
+from django.shortcuts import render, redirect
 # Create your views here.
+from . import authenticate
+
+def index(request):
+    if 'doctor' in request.session:
+        d = utility.get_doctor(request.session['doctor'])
+        appointments = utility.getTodaysAppointments(d)
+        context = {
+                "appointments": appointments
+        }
+        return render(req, "appointmentDashboard.html", context)
+
+    context = {
+        "client_id": authenticate.client_id,
+    }
+    return render(request, "index.html", context)
